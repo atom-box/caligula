1aca81320a6353a96014a4f2b16d10548a80382f
atom-box <atom_box@hotmail.com>
2020-12-13 16:54:34 -0600

    Add intro to top of response.

--- src/Controller/GiveRating.php
+++ src/Controller/GiveRating.php
@@ -6,8 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-
-
 /*
 We are having a party with amounts of tea and cake. 
 Return a rating: bad, good, or great as a string. 
@@ -16,8 +14,6 @@ However, if either tea or cake is at least double the amount of the other one, t
 if either tea or cake is less than 5, the party is always bad (0).
 */
 
-
-
 class GiveRating extends AbstractController
 {
     /**
@@ -25,15 +21,26 @@ class GiveRating extends AbstractController
      * @Route("/teaparty")
      */
 
+
+
     public function teaParty(): Response
     {
+        $cake = 6; $tea = 8;
         $res = '';
-        $res = '<div style="color:yellow;background-color:blue;text-align:center;" ><h2>Yipee...</h2></div>';
+        $res .= '<div style="border-left: 6px solid red;background-color: lightgrey;">
+        <h2>
+        At the tea party we rated the cake and tea ';
+        $res .= $cake;
+        $res .= " and ";
+        $res .= $tea;
+        $res .= ' respectively and respectfully
+        </h2>
+        </div>';
+        $res .= '<div style="color:yellow;background-color:blue;text-align:center;" ><h2>That party rates as </h2></div>';
         return new Response($res);
     }
 }
 
 
-// teaParty(6, 8) → 1
 // teaParty(3, 8) → 0
 // teaParty(20, 6) → 2
