7bce7d6467a8c58f8083c935082337157abd7aa9
atom-box <atom_box@hotmail.com>
2020-12-21 05:37:39 -0600

    David Powers html.  Cannot enter into Fields 1, 2.

--- src/Controller/BestOf.php
+++ src/Controller/BestOf.php
@@ -11,39 +11,44 @@ class BestOf
      * @Route("best-of")
      */
 
-     public function inputTheList(){
-         
-         
-         return new Response(
-             '<html>
-
+    public function inputTheList()
+    {
+        return new Response(
+            '<html>
              <style>
              div.container4 {
                 height: 40em;
                 border: 2px solid red;
                 position: relative }
-                div.container4 p   { width: 60vw; min-width: 360px; text-align: center; margin: 5% auto; background-color: #FEEE33; }
-                div.container4 form   { width: 60vw; min-width: 360px; text-align: center; margin: 5% auto; background-color: #ccc; }
-             
+            div.container4 h1     { width: 60vw; min-width: 360px; text-align: center; margin: 5% auto; background-color: #FEEE33; }
+            div.container4 form   { width: 60vw; min-width: 360px; text-align: center; margin: 5% auto; background-color: #ccc; }
             </style>
+            <body>
+                <div class=container4>
+                    <h1 style="font-size:48px">
+                        &#128466; End of the year Best-of lists &#128334;
+                    </h1>
+                    <form method="post" action="">
+                    <p>
+                        <label for="name">Name:</label><br>
+                        <input name="user-name" id="user-name" type="submit" ></input>
+                    </p>
+                    <p>
+                        <label for="category">Category:</label><br>
+                        <input name="category"  id="category"  type="submit" >
+                    </p>
+                    <p>
+                        <label for="user-submit-list">Your favorites:</label><br>
+                        <textarea name="message" id="user-list" rows="10" cols="30">x</textarea> 
+                    </p>
+                    <p>
+                        <input name="send" type="submit" value="Make"><br>
+                    </p>
 
-             <body>
-<div class=container4>
-            <p style="font-size:48px">
-             &#128466; End of the year Best-of lists &#128334;
-</p>
-<form action="welcome.php" method="post">
-<input type="submit" value="Submit"><br>
-
-<textarea name="message" rows="10" cols="30">
-Paste your list here
-</textarea> 
-
-</form>
-</div>
+                    </form>
+                </div>
              </body>
              </html>'
-            );
-            
-        }
+        );
+    }
 }
