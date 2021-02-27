0232e75c62bdcc935f01ba26a012d25a863747c4
atom-box <atom_box@hotmail.com>
2021-02-27 09:20:48 -0600

    Finish function lessYak.

--- src/Controller/NutsAndBolts.php
+++ src/Controller/NutsAndBolts.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class NutsAndBolts
+interface batWarmup{
+    public function lessYak(string $s): string;
+}
+
+class NutsAndBolts implements batWarmup
 {
     /**
      * @Route("/simple")
      */
-
-
     
     public function hydrogen()
     {
@@ -21,10 +23,32 @@ class NutsAndBolts
             "<h1>Blue $typical[0] Way (b/w)Lasagna for $typical[1]</h1>"
         );
     }
-
+    
     public function nihl($a){
         $b = $a;
         return $b;
         
     }
+    
+    public function lessYak(string $rawString): string{
+        $improvedString = '';
+        $pieces = [];
+        $pattern = '/yak/i';
+        $pieces = preg_split($pattern, $rawString, -1); 
+        $improvedString = implode('', $pieces);
+        return $improvedString;
+    }
+
 }
+
+/*
+LESSYAK
+Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, The "yak" strings will not overlap.
+*/
+
+$solver = new NutsAndBolts;
+echo $solver->lessYak("yakpak")."\n"; // "pak"
+echo $solver->lessYak("pakyak")."\n"; // "pak"
+echo $solver->lessYak("yak123ya")."\n"; // "123ya"
+
+
