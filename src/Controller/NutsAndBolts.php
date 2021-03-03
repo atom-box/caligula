b8d6fff454d16bf46e7972254292f5f309e9e5b9
atom-box <atom_box@hotmail.com>
2021-03-02 22:38:31 -0600

    WIP: call to undefined function in #67.

--- src/Controller/NutsAndBolts.php
+++ src/Controller/NutsAndBolts.php
@@ -49,6 +49,24 @@ class NutsAndBolts implements batWarmup
         }
         return $outstring;
     }
+    
+    public function fixTeen(int $n): int{
+        if(in_array($n, [13, 14, 17, 18, 19])){
+            return 0;
+        }
+
+        return $n;
+    }
+
+    public function noTeenSum(array $ints): int{
+        if(count($ints) === 0){
+            return 0;
+        }
+        $poppedInt = array_pop($ints);
+        echo(count($ints)."\n");
+        return fixTeen($poppedInt) + noTeenSum($ints);
+    }
+
 }
 
 /*
@@ -57,6 +75,9 @@ Suppose the string "yak" is unlucky. Given a string, return a version where all 
 
 STRINGBITS
 Given a string, return a new string made of just the odd chars, so "Hello" yields "Hlo".
+
+NOTEENSUM
+Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "public int fixTeen(int n) {"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main noTeenSum().
 */
 
 $solver = new NutsAndBolts;
@@ -68,3 +89,8 @@ echo $solver->lessYak("yak123ya")."\n"; // "123ya"
 echo $solver->stringBits("Hello")."\n"; //"Hlo"
 echo $solver->stringBits("Hi")."\n"; //"H"
 echo $solver->stringBits("Heeololeo")."\n"; //"Hello"
+
+
+echo $solver->noTeenSum([1, 2, 3]);// 6
+echo $solver->noTeenSum([2, 13, 1]);// 3
+echo $solver->noTeenSum([2, 1, 14]);// 3
