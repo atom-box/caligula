411296cbcd5ffacc7b37f3eeee5c57c90730f898
atom-box <atom_box@hotmail.com>
2021-03-03 05:49:53 -0600

    Start SentenceFormer in services, not controllers.

---
+++ src/Service/SentenceFormer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Service;
+
+class SentenceFormer
+{
+    // return a greeting, name optional
+    public function giveHello(string $toName = ''): string
+    {
+        $messages = [
+            "Hey there, $toName",
+            "Wowzie, $toName",
+            "Que Pas, $toName!",
+        ];
+
+        $index = array_rand($messages);
+
+        return $messages[$index];
+    }
+}
+
+// $talker = new SentenceFormer();
+// echo $talker->giveHello()."\n:::::::\n";
