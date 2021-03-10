9318574a7054a016b648858fee91ff9460b5c221
atom-box <atom_box@hotmail.com>
2021-03-10 05:45:50 -0600

    WIP: start getters, add bool types.

--- src/Controller/KataProduct.php
+++ src/Controller/KataProduct.php
@@ -5,22 +5,40 @@ namespace App\Controller;
 
 class KataProduct
 {
+    private int $productNumber;
+    private float $cost;
+    private float $price;
+    private bool $loseMoney;
+    private bool $bogoOn;
+
     public function __construct(float $cost, float $price, int $productNumber, array $mods = [])
     {
 
         $this->productNumber = $productNumber;
         $this->cost = $cost;
         $this->price = $price;
-        $this->mods = ['numberPay' => 1, 'numberReceive' => 1];
+        $this->mods = ['bogoQuantA' => 1, 'bogoQuantB' => 1];
     }
 
     /*
-    Allows Price to have Paid For & Received number
+    Allows Price to have
+    1) by one (or more)
+    2) get one (or more)
     */
-    public function setBogo(int $productNumber, int $numberPay, int $numberReceive)
+    public function setBogo(int $productNumber, int $bogoQuantA, int $bogoQuantB)
     {
-        $this->mods['numberPay'] = $numberPay;
-        $this->mods['numberReceive'] = $numberReceive;
+        if (stockValueIsNegative() && loseMoney === false){
+            $this->logger("Did not set: BOGO.")
+            $this->logger("Stock value cannot be negative unless loseMoney flag is True.")
+            return;
+        }
+        $this->mods['bogoQuantA'] = $bogoQuantA;
+        $this->mods['bogoQuantB'] = $bogoQuantB;
+    }
+
+    private function stockValueIsNegative(): bool {
+        // todo some checks go here
+        return false;
     }
 }
 
@@ -28,10 +46,15 @@ class KataProduct
 /*
     http://codekata.com/kata/kata01-supermarket-pricing/
 
-
+    The Kata Question was originally stated:
     - three for a dollar (so what’s the price if I buy 4, or 5?)
     - $1.99/pound (so what does 4 ounces cost?)
     - buy two, get one free (so does the third item have a price?)
+
+    My thoughts:
+    - BOGO enabled or not is a bool.
+    - BOGOdenabled needs a checker ("Worth of item cannot become less than zero unless SellAtLoss is True. ")
+    - Sell at loss bool is checked during cost set BOBO set price set, bogoenabled set.
 
 
 */
